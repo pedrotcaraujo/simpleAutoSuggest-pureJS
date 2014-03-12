9c8536fbd395f00a2eddb4bef1bd9f580a7e36f2
Pedro Araujo <pedrotcaraujo@gmail.com>
2014-03-12 14:45:07 -0300

    implement suggest select

--- script.js
+++ script.js
@@ -1,48 +1,68 @@
-	var users = ["Pedro Araujo", "Tony", "Adriana", "Rafael Melo", "Rafaela Fernanda", "Luis Fernando"];
-	var autoSuggest = document.querySelector('#auto-suggest');
-	var suggests = document.querySelector('.suggests');
-	var suggestList = [];
+var USERS = ["Pedro Araujo", "Tony", "Adriana", "Rafael Melo", "Rafaela Fernanda", "Luis Fernando"];
+	
+var autoSuggest = document.querySelector('#auto-suggest');
+var suggests = document.querySelector('.suggests');
+var suggestList = [];
 
-	autoSuggest.onkeyup = function () {
-		resetSuggest();
-		if (autoSuggest.value.length >= 3) {
-			searchUsers(autoSuggest.value);
-		}
-	}	
+autoSuggest.onkeyup = function () {
+	resetSuggest();
+	if (autoSuggest.value.length >= 3) {
+		searchUsers(autoSuggest.value);
+	}
+}	
 
-	function searchUsers(autoSuggestValue) {
-		var re = new RegExp(autoSuggestValue,'i');		
+function searchUsers(autoSuggestValue) {
+	var re = new RegExp(autoSuggestValue,'i');		
 
-		for (var i = users.length - 1; i >= 0; i--) {
-			if (users[i].match(re) != null) {
+	for (var i = USERS.length - 1; i >= 0; i--) {
+		if (USERS[i].match(re) != null) {
 
-				checkSuggestOnList(users[i]);
-				updateSuggestList();
-			};
+			checkSuggestOnList(USERS[i]);
+			updateSuggestList();
+			buildLinkItemSuggest();		
 		};
-	}
+	};
+}
 
-	function checkSuggestOnList(suggest) {
-		if (suggestList.length > 0) {
-			for (var i = suggestList.length - 1; i >= 0; i--) {
-				if (suggestList[i] != suggest) {
-					suggestList.push(suggest);
-				};
-			};
-		} else {
-			suggestList.push(suggest);
-		}
-	}
-
-	function updateSuggestList() {
-		suggests.innerHTML = "";
+function checkSuggestOnList(suggest) {
+	if (suggestList.length > 0) {
 		for (var i = suggestList.length - 1; i >= 0; i--) {
-			suggests.innerHTML += "<p>"+suggestList[i]+"</p>";
+			if (suggestList[i] != suggest) {
+				suggestList.push(suggest);
+			}
 		};
+	} else {
+		suggestList.push(suggest);
 	}
+}
 
-	function resetSuggest() {
-		suggests.innerHTML = "";
-		suggestList.length = 0;
-	}
+function updateSuggestList() {
+	suggests.innerHTML = "";
+	suggests.style.display = "none";
+	for (var i = suggestList.length - 1; i >= 0; i--) {
+		if (i == suggestList.length - 1) {
+			suggests.style.display = "block";
+		};
+		suggests.innerHTML += "<li class='item-suggest'>"+suggestList[i]+"</li>";
+	};
+}
+
+function buildLinkItemSuggest() {
+	if (document.querySelectorAll('.item-suggest').length > 0) {
+		var itemSuggest = document.querySelectorAll('.item-suggest');
+		for (var i = itemSuggest.length - 1; i >= 0; i--) {			
+			itemSuggest[i].onclick = function() {
+				autoSuggest.value = this.innerText;
+				resetSuggest();
+			}
+		};
+	};
+}
+
+function resetSuggest() {
+	suggests.innerHTML = "";
+	suggestList.length = 0;
+	suggests.style.display = "none";
+}
+
 
